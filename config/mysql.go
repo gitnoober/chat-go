0f9ae78b4c538166d3ef4c84b2fecefdd82341f7
Priyanshu Singh <priyanshusingh349@gmail.com>
2024-09-30 23:43:22 +0530

    added logs

--- config/mysql.go
+++ config/mysql.go
@@ -43,6 +43,7 @@ func ConnectMysql(
 	// Get a database handle
 	var err error
 	dsn := mysqlConfig.FormatDSN()
+	log.Println("cfg.DBConfig:", cfg.DBConfig)
 	log.Println("Connecting to database with DSN:", dsn)
 
 	for i := 0; i < 5; i++ {
